e75434e06eb8d9668d1d94f545aa597996600abd
hongjeongseob <hongjeongseob82@gmail.com>
2020-10-11 22:01:37 +0900

    Part 3 | Add to Cart Functionality

--- store/views.py
+++ store/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
-from .models import Product, Order
+from .models import Product, Order, OrderItem, ShippingAddress
+from django.http import JsonResponse
+import json
+import datetime
 
 # Create your views here.
 def store(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {"get_cart_total": 0, "get_cart_items": 0}
+        cartItems = order["get_cart_items"]
+
     products = Product.objects.all()
-    context = {"products": products}
+    context = {"products": products, "cartItems": cartItems, "shipping": False}
     return render(request, "store/store.html", context)
 
 
@@ -13,11 +27,18 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
         order = {"get_cart_total": 0, "get_cart_items": 0}
+        cartItems = order["get_cart_items"]
 
-    context = {"items": items, "order": order}
+    context = {
+        "items": items,
+        "order": order,
+        "cartItems": cartItems,
+        "shipping": False,
+    }
     return render(request, "store/cart.html", context)
 
 
@@ -26,9 +47,68 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
     else:
         items = []
-        order = {"get_cart_total": 0, "get_cart_items": 0}
+        order = {"get_cart_total": 0, "get_cart_items": 0, "shipping": False}
+        cartItems = order["get_cart_items"]
 
-    context = {"items": items, "order": order}
+    context = {"items": items, "order": order, "cartItems": cartItems}
     return render(request, "store/checkout.html", context)
+
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data["productId"]
+    action = data["action"]
+    print("Action", action)
+    print("Product", productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, orderCreated = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, orderitemCreated = OrderItem.objects.get_or_create(
+        product=product, order=order
+    )
+
+    if action == "add":
+        orderItem.quantity = orderItem.quantity + 1
+    elif action == "remove":
+        orderItem.quantity = orderItem.quantity - 1
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse("Item was added", safe=False)
+
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data["form"]["total"])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data["shipping"]["address"],
+                city=data["shipping"]["city"],
+                state=data["shipping"]["state"],
+                zipcode=data["shipping"]["zipcode"],
+            )
+
+    return JsonResponse("Payment complete", safe=False)
+
+    # return JsonResponse({"Payment complete": 1111, "second": 12})
